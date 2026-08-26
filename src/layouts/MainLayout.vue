21f9cacaa2052ce3fdb79a36dd19a82fcda3708a
kentangkotak <ariezlnd69@gmail.com>
2026-08-27 00:56:19 +0700

    sasas

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,19 +2,13 @@
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
       <q-toolbar>
-        <q-btn flat dense round icon="menu" aria-label="Menu" @click="toggleLeftDrawer" />
-
-        <q-toolbar-title> ORADO CLUB </q-toolbar-title>
+        <q-avatar size="30px" class="header-logo"><img src="@/assets/orado/logo-white.svg" alt="ORADO" /></q-avatar>
+        <q-toolbar-title>ORADO CLUB</q-toolbar-title>
+        <q-btn flat round dense icon="notifications_none" aria-label="Notifikasi">
+          <q-badge floating rounded color="red" label="3" />
+        </q-btn>
       </q-toolbar>
     </q-header>
-
-    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
-      <q-list>
-        <q-item-label header> Essential Links </q-item-label>
-
-        <EssentialLink v-for="link in linksList" :key="link.label" v-bind="link" />
-      </q-list>
-    </q-drawer>
 
     <q-page-container>
       <router-view />
@@ -22,65 +16,10 @@
   </q-layout>
 </template>
 
-<script setup>
-import { ref } from 'vue'
-import EssentialLink from '@/components/EssentialLink.vue'
-
-const linksList = [
-  {
-    label: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev',
-  },
-  {
-    label: 'GitHub',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework',
-  },
-  {
-    label: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev',
-  },
-  {
-    label: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev',
-  },
-  {
-    label: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev',
-  },
-  {
-    label: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev',
-  },
-  {
-    label: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev',
-  },
-]
-
-const leftDrawerOpen = ref(false)
-
-function toggleLeftDrawer() {
-  leftDrawerOpen.value = !leftDrawerOpen.value
-}
-
-</script>
-
 <style scoped>
 .q-header { background: #006b4f; }
 .q-toolbar { min-height: 58px; }
 .q-toolbar-title { font-size: 14px; font-weight: 700; }
+.header-logo { display: grid; margin-left: 4px; place-items: center; background: rgba(255,255,255,.1); }
+.header-logo img { width: 25px; height: 25px; object-fit: contain; }
 </style>
